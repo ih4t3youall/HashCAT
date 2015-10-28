c43e669aa33b246f1ca00766b5c30cb177a377f6
ih4t3youall <sourcesistemas@gmail.com>
2015-10-28 18:09:35 -0300

    campo texto 2 disabled

--- src/ar/com/hasmap/vista/MainFrame.java
+++ src/ar/com/hasmap/vista/MainFrame.java
@@ -39,6 +39,7 @@ public class MainFrame extends JFrame {
 		panelPrincipal.setLayout(new FlowLayout());
 		campoTexto = new JTextField(20);
 		campoTexto2 = new JTextField(20);
+		campoTexto2.setEnabled(false);
 		buscar = new JButton("Buscar");
 		
 		
